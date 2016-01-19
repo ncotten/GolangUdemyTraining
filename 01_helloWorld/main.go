f6f01ab0a06669eeb34c9878da99f4760afa012e
Nicholas Cotten <nicholasdcotten@gmail.com>
2016-01-19 00:47:41 -0500

    Show Various Forms of Variable Types

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("42")
-}
+	fmt.Printf("%d - %b - %x\n", 42, 42, 42) // Print decimal, binary, hexadecimal
 
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+	}
+}
